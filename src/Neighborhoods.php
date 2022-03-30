a307f671746e7bdaf302d4032ca52314a1a68546
Adam Balsam <balsam.adam@gmail.com>
2022-03-29 20:25:44 -0400

    Make all neighborhoods arrays of polygons

--- src/Neighborhoods.php
+++ src/Neighborhoods.php
@@ -15,24 +15,19 @@ class Neighborhoods
         $bostonGeoJson = json_decode(file_get_contents(__DIR__ . '/../geojson/Boston_Neighborhoods.geojson'));
 
         foreach ($bostonGeoJson->features as $rawNeighborhoodFeatures) {
-            if ($rawNeighborhoodFeatures->geometry->type === 'MultiPolygon') {
-                $polygons = [];
-                foreach ($rawNeighborhoodFeatures->geometry->coordinates as $polygon) {
-                    $neighborhoodPolygonN = new Polygon();
-                    foreach ($polygon[0] as $coordinate) {
-                        $neighborhoodPolygonN->addPoint(new Coordinate($coordinate[0], $coordinate[1]));
-                    }
-                    $polygons[] = $neighborhoodPolygonN;
+            $polygons = [];
+            foreach ($rawNeighborhoodFeatures->geometry->coordinates as $polygon) {
+                $neighborhoodPolygonN = new Polygon();
+                if (count($polygon) === 1) {
+                    // Multi-polygon neighborhoods have their coordinates nested one level deeper for some reason.
+                    $polygon = reset($polygon);
                 }
-                $neighborhoodPolygon = $polygons;
-            }
-            elseif ($rawNeighborhoodFeatures->geometry->type === 'Polygon') {
-                $coordinates = reset($rawNeighborhoodFeatures->geometry->coordinates);
-                $neighborhoodPolygon = new Polygon();
-                foreach ($coordinates as $coordinate) {
-                    $neighborhoodPolygon->addPoint(new Coordinate($coordinate[0], $coordinate[1]));
+                foreach ($polygon as $coordinate) {
+                    $neighborhoodPolygonN->addPoint(new Coordinate($coordinate[0], $coordinate[1]));
                 }
+                $polygons[] = $neighborhoodPolygonN;
             }
+            $neighborhoodPolygon = $polygons;
             $neighborhoods[$rawNeighborhoodFeatures->properties->Name] = $neighborhoodPolygon;
         }
         $this->neighborhoods = $neighborhoods;
